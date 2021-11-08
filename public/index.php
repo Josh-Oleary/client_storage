bb556a78506325c871c379f7a5e347ca78973d4e
Josh <jdoleary91@gmail.com>
2021-11-08 10:19:28 -0500

    fix sql insert bug

--- public/index.php
+++ public/index.php
@@ -10,4 +10,29 @@ $app = new \Slim\App;
 
 require '../src/routes/clients.php';
 
+$app->get('/{file}', function (Request $request, Response $response, $args) {
+  $filePath = __DIR__ . '/' . $args['file'];
+
+  if (!file_exists( $filePath )) {
+      return $response->withStatus(404, 'File Not Found');
+  }
+
+  switch (pathinfo( $filePath, PATHINFO_EXTENSION )) {
+      case 'css':
+          $mimeType = 'text/css';
+      break;
+      case 'js':
+          $mimeType = 'application/javascript';
+      break;
+      default:
+          $mimeType = 'text/html';
+  }
+
+  $newResponse = $response->withHeader('Content-Type', $mimeType . '; charset=UTF-8');
+
+  $newResponse->getBody()->write(file_get_contents( $filePath ));
+
+  return $newResponse;
+});
+
 $app->run();
--- src/routes/clients.php
+++ src/routes/clients.php
@@ -4,10 +4,6 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app = new \Slim\App;
 
-$app->get('/', function (Request $request, Response $response) {
-  $response->getBody()->write("Home Page");
-  return $response;
-});
 
 $app->get('/clients', function (Request $request, Response $response) {
   $sql = "SELECT * FROM clients";
@@ -16,17 +12,38 @@ $app->get('/clients', function (Request $request, Response $response) {
     $db = new db();
     $db = $db->connect();
 
-    $stmt = $db->query($sql);
+    $stmt = $db->query( $sql );
     $clients = $stmt->fetchAll(PDO::FETCH_OBJ);
     $db = null;
-    echo json_encode($clients);
+    echo json_encode( $clients );
   } catch(PDOException $e) {
     echo '{"error": {"text": '.$e->getMessage().'}}';
   }
 });
 
-$app->get('/add', function(Request $request, Response $response) {
-  echo 'ADD CLIENT';
+$app->post('/clients/add', function(Request $request, Response $response) {
+  $first_name = $request->getParam('first_name');
+  $last_name = $request->getParam('last_name');
+  $emails = $request->getParam('email');
+
+  $sql = "INSERT INTO `clients` (first_name, last_name) VALUES (:first_name, :last_name);";
+  
+  try{
+    $db = new db();
+    $db = $db->connect();
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(':first_name', $first_name);
+    $stmt->bindParam(':last_name', $last_name);
+
+    $stmt->execute();
+
+    echo 'Add client success';
+
+  } catch(PDOException $e) {
+    echo '{"error": {"text": '.$e->getMessage().'}}';
+  }
 });
 
 $app->get('/clients/{id}', function(Request $request, Response $response) {
@@ -37,10 +54,10 @@ $app->get('/clients/{id}', function(Request $request, Response $response) {
     $db = new db();
     $db = $db->connect();
 
-    $stmt = $db->query($sql);
-    $clients = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $stmt = $db->query( $sql );
+    $client = $stmt->fetchAll(PDO::FETCH_OBJ);
     $db = null;
-    echo json_encode($clients);
+    echo json_encode( $client );
   } catch(PDOException $e) {
     echo '{"error": {"text": '.$e->getMessage().'}}';
   }
